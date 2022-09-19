0e6891efc4293f2e26a762130bf3ac5752a9fa85
luckyprasu22 <113031748+luckyprasu22@users.noreply.github.com>
2022-09-19 18:52:26 +0530

    Add files via upload

---
+++ perimeter_of_rectangle.py
@@ -0,0 +1,7 @@
+#python program to find the perimeter of rectangle
+print("Enter anything for the value ")
+l = int(input())
+b = int(input())
+#calculate the perimeter
+perimeter = 2*(l+b)
+print('the perimeter of the rectangle is %d '%perimeter)
