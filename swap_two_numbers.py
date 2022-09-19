7d0b0d5b6b25fe1aa0653c4fa2cfcdbea9e2cb78
luckyprasu22 <113031748+luckyprasu22@users.noreply.github.com>
2022-09-19 18:55:15 +0530

    Add files via upload

---
+++ swap_two_numbers.py
@@ -0,0 +1,9 @@
+#python program to swap two variables
+x = 5
+y = 10
+#create a temporaray variable and swap the value
+temp = x
+x = y
+y = temp
+print('The value of x after swapping: ',x)
+print('The value of y after swapping: ',y)
