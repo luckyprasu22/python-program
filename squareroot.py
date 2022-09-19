3dbcbb447fabe137308866ad26fcfa9051b5fe33
luckyprasu22 <113031748+luckyprasu22@users.noreply.github.com>
2022-09-19 18:54:14 +0530

    Add files via upload

---
+++ squareroot.py
@@ -0,0 +1,8 @@
+#python program to calculate the square root
+#Note:change this value for a different result
+num = int(input("Enter number"))
+#To take the input from the user
+#num = float(input('Enter a number: '))
+num_sqrt = num**0.5
+print("The value of num_sqrt is ",num_sqrt)
+print("The squareroot of%d is %d% (num,num_sqrt)")
