15195698b79f816260804d17551fd4f49bc11faa
luckyprasu22 <113031748+luckyprasu22@users.noreply.github.com>
2022-09-19 18:52:02 +0530

    Add files via upload

---
+++ perimeter_of_circle.py
@@ -0,0 +1,7 @@
+#python program to find the perimeter of circle
+print("Enter anything for the value")
+pi = 3.14
+r = int(input())
+#calculate the perimeter
+perimeter = 2*pi*r
+print('the perimeter of the circle is %d'%perimeter)
