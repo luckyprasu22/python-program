53d32d1dbb09f070f6435fff4d698ea82908dc8d
luckyprasu22 <113031748+luckyprasu22@users.noreply.github.com>
2022-09-19 18:37:52 +0530

    Add files via upload

---
+++ area_of_circle.py
@@ -0,0 +1,7 @@
+#python program to find the area of circle
+print("Enter anything for the value")
+pi = 3.14
+r = int(input())
+#calculate the area
+area = pi*r*r
+print('the area of the circle is %d'%area)
