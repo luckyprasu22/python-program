5ddc4de3687b9654347d63f86b4e5ebf2c02bfbc
luckyprasu22 <113031748+luckyprasu22@users.noreply.github.com>
2022-09-19 18:41:42 +0530

    Add files via upload

---
+++ data_type.py
@@ -0,0 +1,11 @@
+i = 7
+c = 24+8j
+f = 701
+s = 'HELLO EVERYONE!!\n This is john\s python programing'
+b = True
+print("the value of i is:",i,'\n its type is:', type(i))
+print("the value of c is:",c,'\n its typoe is:',type(c))
+print("the value of f is:",f,'\n its type is:', type(f))
+print("the value of s is:",s,'\n its type is:', type(s))
+print("the value of b is:",b,'\n its type is:', type(b))
+print('Note: boolean has truth values that they sensitive EX: True(T is caps!)')
