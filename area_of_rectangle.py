306e6e2392ebacab9d4b0f45d8df50eb61e373b1
luckyprasu22 <113031748+luckyprasu22@users.noreply.github.com>
2022-09-19 18:38:19 +0530

    Add files via upload

---
+++ area_of_rectangle.py
@@ -0,0 +1,7 @@
+#python program to find the area of rectangle
+print("Enter anything for the value")
+l = int(input())
+b = int(input())
+#calculate the area
+area = l*b
+print('the area of the rectangl is %d' %area)
