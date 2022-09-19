1a3898381b92eb15c679de3c4225ef33e4636030
luckyprasu22 <113031748+luckyprasu22@users.noreply.github.com>
2022-09-19 18:46:27 +0530

    Add files via upload

---
+++ integer_multiplication.py
@@ -0,0 +1,7 @@
+#Multiplying two numbers in python
+print("Enter First Number")
+numOne = int(input())
+print("Enter Second Number")
+numTwo = int(input())
+res = numOne*numTwo
+print("\n Result:",res)
