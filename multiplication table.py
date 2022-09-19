d904c260e6cbe39ae8d89730c3c126b8689e85df
luckyprasu22 <113031748+luckyprasu22@users.noreply.github.com>
2022-09-19 18:48:36 +0530

    Add files via upload

---
+++ multiplication table.py
@@ -0,0 +1,10 @@
+#this is a program to display multiplication table
+#example for nested whileloop in python
+i = 1
+while i<=10:
+    j = 1
+    while j<=12:
+        print(i*j,end="")
+        j+=1
+    i+=1
+    print()
