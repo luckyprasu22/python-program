a2bf9bc89c7f90a32ba85e8dfbb4490440910290
luckyprasu22 <113031748+luckyprasu22@users.noreply.github.com>
2022-09-19 18:38:55 +0530

    Add files via upload

---
+++ area_of_triangle_without_data.py
@@ -0,0 +1,7 @@
+#python program to find the area of triangle
+print("Enter anything for the value")
+b = int(input())
+h = int(input())
+#calculate the area
+area = (0.5*b*h)
+print('the area of the triangle is%f' %area)
