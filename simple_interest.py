523881af84029547f85f138780b93a2bbd121529
luckyprasu22 <113031748+luckyprasu22@users.noreply.github.com>
2022-09-19 18:53:32 +0530

    Add files via upload

---
+++ simple_interest.py
@@ -0,0 +1,6 @@
+#python program to calculate simple interest
+p = int(input("enter number"))
+t = int(input("enter number"))
+r = int(input("enter number"))
+simpleinterest =  p*t*r/100
+print("simpleinterest is%d",simpleinterest)
