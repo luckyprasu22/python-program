cb7486e931565b025763aba22dcf93daf70994d4
luckyprasu22 <113031748+luckyprasu22@users.noreply.github.com>
2022-09-19 18:45:32 +0530

    Add files via upload

---
+++ if-else in while loop.py
@@ -0,0 +1,9 @@
+n = int(input("Please enter number"))
+while n>0:
+    #check even and odd
+    if n%2 == 0:
+        print(n,'is a even number')
+    else:
+        print(n,'is a odd number')
+#decrease number by 1in each iteration
+    n = n-1
