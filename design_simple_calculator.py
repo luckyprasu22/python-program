7cd3e6a8aed82ecf7a76eea72f23b787e8f99237
luckyprasu22 <113031748+luckyprasu22@users.noreply.github.com>
2022-09-19 18:41:58 +0530

    Add files via upload

---
+++ design_simple_calculator.py
@@ -0,0 +1,19 @@
+#python program to make a simple caluclator
+print('''please select the operation you want to perform
+      1 = Add
+      2 = Subtract
+      3 = Multiply
+      4 = Divide''')
+opt = int(input("Choose Operation from 1 , 2 , 3 , 4 = "))
+n1 = int(input("First Number = "))
+n2 = int(input("Second Number= "))
+if opt == 1:
+    print(n1, '+' , n2, '=' , n1+n2)
+elif opt == 2:
+    print(n1, '-' , n2, '=' , n1-n2)
+elif opt == 3:
+    print(n1, '*' , n2, '=' , n1*n2)
+elif opt == 4:
+    print(n1, '/' , n2, '=' , n1/n2)
+else:
+    print('Invalid Input')
