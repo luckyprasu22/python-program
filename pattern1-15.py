f6a0b6b35b8f2caf549c551dd9ccca1f2b188e64
luckyprasu22 <113031748+luckyprasu22@users.noreply.github.com>
2022-09-19 18:51:38 +0530

    Add files via upload

---
+++ pattern1-15.py
@@ -0,0 +1,14 @@
+#python program to print number pattern using while loop
+n = int(input("Enter number of rows: "))
+k = 1
+i = 1
+while i<=n:
+    j = 1
+    while j<+i:
+        print(k,end= "")
+        j+=1
+        k+=1
+    print()
+    i+=1
+            
+              
