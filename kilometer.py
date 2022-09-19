5a8b121bbd1fca4706fb15071751f269a636d5ee
luckyprasu22 <113031748+luckyprasu22@users.noreply.github.com>
2022-09-19 18:47:16 +0530

    Add files via upload

---
+++ kilometer.py
@@ -0,0 +1,5 @@
+kilometer_1 = float(input("please enter the speed of car in kilometer as a unit:"))
+conversion_ratio_1 = 0.621371
+miles_1 = kilometer_1*conversion_ratio_1
+print("the speed value of car in miles: ",miles_1)
+                          
