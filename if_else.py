f46f9211057844400600649393b52fb5d5c2f1c3
luckyprasu22 <113031748+luckyprasu22@users.noreply.github.com>
2022-09-19 18:45:14 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,10 @@
+#program checks if the number is positive or negative
+#and displays an appropriate message
+num = 3
+#Try these two variations as well
+#num = -5
+#num = 0
+if num>=0:
+    print("positive or zero")
+else:
+    print("Negative number")
