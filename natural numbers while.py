16a1ec630a3289e5b4e58ebfc9beda0b8a7132e1
luckyprasu22 <113031748+luckyprasu22@users.noreply.github.com>
2022-09-19 18:49:19 +0530

    Add files via upload

---
+++ natural numbers while.py
@@ -0,0 +1,6 @@
+number = int(input("Please enter any number"))
+i = 5
+print("the list of Natural numbers from 1 to number are format(number)")
+while (i<=number):
+    print(i,end = '')
+    i = i+1
