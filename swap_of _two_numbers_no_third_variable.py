3abdd4e64d4608aa05929f8ed0f7a04f22e715a7
luckyprasu22 <113031748+luckyprasu22@users.noreply.github.com>
2022-09-19 18:54:57 +0530

    Add files via upload

---
+++ swap_of _two_numbers_no_third_variable.py
@@ -0,0 +1,9 @@
+#python program to swap the two numbers without using third componenet
+x = 5
+y = 10
+x = ('x + y = 5 + 10 = 15')
+y = ('x - y = 15 - 10 = 5')
+x = ('x - y = 15 - 5 = 10')
+print('The value of x after swapping: ',x)
+print('The value of y after swapping:',y)
+     
