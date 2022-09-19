337ec5a671785d94cd701e3147b9c4a27b970528
luckyprasu22 <113031748+luckyprasu22@users.noreply.github.com>
2022-09-19 18:40:31 +0530

    Add files via upload

---
+++ concatinate_to_tring.py
@@ -0,0 +1,10 @@
+pi = 3.14
+s = "venkata"
+v = "subramanyam"
+print("the value of s is:",s)
+print("the value of v is:",v)
+string_add = s+v
+print("after cocatenating s and v the string is:",s+v)
+text = 'The value of pi is' + str(pi)
+print("Note: Variables after '+' operator must be converted to string before using them as strings/notherwise value will be considered as its class type")
+print(text)
